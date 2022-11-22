b87fab181a11836c586aa192ca39eeffe9e22f67
Ryan-Whitney <ryan.whitney@unb.ca>
2022-11-22 17:13:37 -0400

    Scratch files

---
+++ tester.py
@@ -0,0 +1,64 @@
+# Ryan Whitney
+# Offical python-chess docs code was used
+# https://python-chess.readthedocs.io/en/latest/pgn.html
+
+# first_game.headers["Result"]
+# can read header metadata
+
+# has_castling_rights
+# can see if a side can castle
+
+import chess.pgn
+
+# Open PGN
+pgn = open("chessgames2.txt")
+#print(first_game.headers["Result"])
+
+# Lookup table for piece values
+dict = {'r': 5, 'R': 5, 'n': 3,'N': 3, 'b': 3, 'B': 3, 'q': 9,'Q': 9, 'k': 0, 'K': 0, 'p': 1,'P': 1}
+totalMaterialWhite = 0
+totalMaterialBlack = 0
+
+# Iterate through all moves and play them on a board.
+game1 = chess.pgn.read_game(pgn)
+board = game1.board()
+currentMove = 0;
+for move in game1.mainline_moves():
+    currentMove = currentMove + 1
+    board.push(move)
+    #print(board)
+    #print()
+    fen = board.fen()
+    #print(fen)
+    #print()
+    totalMaterialWhite = 0
+    totalMaterialBlack = 0
+    totalMaterialWhiteArray = []
+    totalMaterialBlackArray = []
+    for i in fen:
+        if not (i.isnumeric()) and i != "/":
+            if i == ' ':
+                break
+            else:
+                if str.islower(i):
+                    # Add to black material total
+                    totalMaterialBlack = totalMaterialBlack + dict[i]
+                else:
+                    # Add to white material total
+                    totalMaterialWhite = totalMaterialWhite + dict[i]
+    # Iterated through this positions FEN, add to array
+    totalMaterialWhiteArray[move] = totalMaterialWhite
+    totalMaterialBlackArray[move] = totalMaterialBlack
+
+
+            # print(i) # prints the fen 1 by 1
+    print(str(totalMaterialWhite))
+    print(str(totalMaterialBlack))
+
+
+print()
+#print(str(totalMaterialWhite))
+print()
+#print(str(totalMaterialBlack))
+
+#get total number of piece material
