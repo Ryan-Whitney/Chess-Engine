070bd9214095bef388e0975f72c57b3887deeff6
Ryan-Whitney <ryan.whitney@unb.ca>
2022-11-23 22:38:27 -0400

    More files

---
+++ model.py
@@ -0,0 +1,64 @@
+# Ryan Whitney
+# Offical python-chess docs code was used
+# https://python-chess.readthedocs.io/en/latest/pgn.html
+
+import csv
+import chess.pgn
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import sklearn
+from sklearn.utils import shuffle
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+temp = []
+with open('gameResult.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    for line in csv_reader:
+        # process each line
+        #print(line)
+        temp.append(line)
+        var1 = ([list(map(int, i)) for i in temp])  # var1 is a 2d list of ints
+        var2 = sum(var1, [])
+
+
+
+#  var is the game result, 1 or 0
+print(var2)
+
+totalMaterialList = []
+with open('totalMaterial.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    for line in csv_reader:
+        # process each line
+        #print(line)
+        totalMaterialList.append(line)
+        var3 = ([list(map(int, i)) for i in totalMaterialList])  # var3 is a 2d list of the totalMaterial for each game
+
+#var3 =  ( [list( map(int,i) ) for i in totalMaterialList] )
+#var4 = sum(var3, [])
+#print(var4)
+print(var3)
+white = var3[0]
+black = var3[1]
+
+
+
+
+# var1 - game results -- 1d array (val for each game)
+# var3 - total material for each move of each game -- 2d array (array of vals for each game)
+
+xLabels = var2
+yFeatures = var3
+
+#x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(xLabels, yFeatures, test_size = 0.1)
+
+
+
+
+
+
+
+
+
--- tester.py
+++ tester.py
@@ -2,9 +2,6 @@
 # Offical python-chess docs code was used
 # https://python-chess.readthedocs.io/en/latest/pgn.html
 
-# first_game.headers["Result"]
-# can read header metadata
-
 # has_castling_rights
 # can see if a side can castle
 
@@ -12,7 +9,11 @@ import chess.pgn
 import csv
 
 # To clear file on run
-with open('testdata.csv', 'w') as f:
+with open('totalMaterial.csv', 'w') as f:
+    # create the csv writer
+    writer = csv.writer(f)
+
+with open('gameResult.csv', 'w') as f:
     # create the csv writer
     writer = csv.writer(f)
 
@@ -24,24 +25,30 @@ dict = {'r': 5, 'R': 5, 'n': 3,'N': 3, 'b': 3, 'B': 3, 'q': 9,'Q': 9, 'k': 0, 'K
 totalMaterialWhite = 0
 totalMaterialBlack = 0
 
-# Iterate through all moves and play them on a board.
-#game1 = chess.pgn.read_game(pgn)
-#print(game1)
-#board = game1.board()
-
 
 # Code section to get result of game
-def gameResult():
-    if currentGameData.headers["Result"] == '0-1': # Black wins
-        pass
-    elif currentGameData.headers["Result"] == '1-0': # White wins
-        pass
-    else: # Draw
-        pass
+def gameResult(currentGameResult):
+    if currentGameResult == '0-1': # Black wins
+        with open('gameResult.csv', 'a', newline='') as f:
+            # create the csv writer
+            writer = csv.writer(f)
+
+            # write a row to the csv file
+            writer.writerow(str(0))
+    elif currentGameResult == '1-0': # White wins
+        with open('gameResult.csv', 'a', newline='') as f:
+            # create the csv writer
+            writer = csv.writer(f)
+
+            # write a row to the csv file
+            writer.writerow(str(1))
+    else:  # Draw
+        pass  # Add error/draw compatibility
+
 
 # Code section to write array of total material throughout game to csv file
 def writeToCSV():
-    with open('testdata.csv', 'a', newline='') as f:
+    with open('totalMaterial.csv', 'a', newline='') as f:
         # create the csv writer
         writer = csv.writer(f)
 
@@ -52,7 +59,7 @@ def writeToCSV():
 totalMaterialWhiteArray = []
 totalMaterialBlackArray = []
 whiteTurn = True
-numberOfGamesInPGN = 8
+numberOfGamesInPGN = 1
 
 #  Iterate through all games
 for i in range(numberOfGamesInPGN):
@@ -60,17 +67,13 @@ for i in range(numberOfGamesInPGN):
     currentGameData = chess.pgn.read_game(pgn)
     board = currentGameData.board()
 
-    gameResult()
-    #print(currentGameData.headers["Result"])
+    gameResult(currentGameData.headers["Result"])
+    #  print(currentGameData.headers["Result"])
 
     #  Iterate through all moves on current game
     for move in currentGameData.mainline_moves():
         board.push(move)
-        #  print(board)
-        #  print()
         fen = board.fen()
-        #  print(fen)
-        #  print()
         totalMaterialWhite = 0
         totalMaterialBlack = 0
 
@@ -84,12 +87,12 @@ for i in range(numberOfGamesInPGN):
                         totalMaterialBlack = totalMaterialBlack + dict[i]  # Add to black material total
                     else: # If its uppercase it is a white piece
                         totalMaterialWhite = totalMaterialWhite + dict[i] # Add to white material total
-        # Iterated through this positions FEN, add to array -- array of total material at each move -- only add when it's
-        # the given player (colour white/black) moved
+        # Iterated through this positions FEN, add to array -- array of total material at each move -- only add when
+        # it's the given player (colour white/black) moved
         if whiteTurn:
-            totalMaterialWhiteArray.append(((int(totalMaterialWhite))))
+            totalMaterialWhiteArray.append(int(totalMaterialWhite))
         else:
-            totalMaterialBlackArray.append(((int(totalMaterialBlack))))
+            totalMaterialBlackArray.append(int(totalMaterialBlack))
 
         whiteTurn = not(whiteTurn) # Change turns white/black
 
@@ -99,6 +102,45 @@ for i in range(numberOfGamesInPGN):
     totalMaterialBlackArray = []
 
 
-#get total number of piece material
+#  get total number of piece material
+
+
+
+
+import numpy as np
+def euclidean_distance(x1, x2):
+    distance = np.sqrt(np.sum((x1 - x2) ** 2))
+    return distance
+
+
+
+gameResultTemp = []
+
+with open('gameResult.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    for line in csv_reader:
+        # process each line
+        #print(line)
+        gameResultTemp.append(line)
+        var1 = ([list(map(int, i)) for i in gameResultTemp])
+        var2 = sum(var1, [])
+
+print(var2)
+
+totalMaterialTemp = []
+
+with open('totalMaterial.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    for line in csv_reader:
+        # process each line
+        #print(line)
+        totalMaterialTemp.append(line)
+        var3 = ([list(map(int, i)) for i in totalMaterialTemp])
+
+print(var3)
+
+#  var is the game result, 1 or 0
+#print(var[5])
+
 
 
